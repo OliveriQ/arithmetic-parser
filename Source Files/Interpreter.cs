8f4ae6694e683d1ca1700ccf43486bebc3f48047
OliveriQ <76691188+OliveriQ@users.noreply.github.com>
2021-10-03 16:12:31 +0100

    Update Interpreter.cs

--- Source Files/Interpreter.cs
+++ Source Files/Interpreter.cs
@@ -59,18 +59,7 @@ namespace arithmeticParser
 
         public void display()
         {
-            Console.Write("[");
-            for (int i = 0; i < _tokens.Count; i++)
-            {
-                if (i < _tokens.Count - 1)
-                    Console.Write(_tokens[i].getValue() + ", ");
-                else
-                    Console.Write(_tokens[i].getValue());
-            }
-            Console.Write("]");
-            Console.WriteLine();
-
-            Console.WriteLine("Result: " + _result);
+            Console.WriteLine(_result);
         }
     }
 }
